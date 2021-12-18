e09609d78ac120ca745224e35a0f30e551cac4ce
Hossain Alinaqi <hossain.alinaqi@ontariotechu.net>
2021-12-18 16:25:19 -0500

    Loading Screen Completed

--- Assets/Scripts/ConnectToServer.cs
+++ Assets/Scripts/ConnectToServer.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.SceneManagement;
 
 public class ConnectToServer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
+
+        SceneManager.LoadScene("Start");
     }
 
     // Update is called once per frame
