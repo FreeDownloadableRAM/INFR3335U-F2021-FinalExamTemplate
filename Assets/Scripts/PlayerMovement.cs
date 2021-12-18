ec3cca76bcd6e66f8f2248ddeffb9e9263be8a34
Hossain Alinaqi <hossain.alinaqi@ontariotechu.net>
2021-12-18 18:22:25 -0500

    movement works with one player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -28,48 +29,53 @@ public class PlayerMovement : MonoBehaviour
     //camera look at
     //private float camDirX, camDirZ, camDirY;
 
+    //Photon
+    PhotonView view;
+
     // Start is called before the first frame update
     void Start()
     {
         //get rigidbody component
         //rb = GetComponent<Rigidbody>();
 
+        //Photon
+        view = GetComponent<PhotonView>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //movement
-
-        dirX = movementJoystick.Horizontal * movementSpeed;
-        dirZ = movementJoystick.Vertical * movementSpeed;
-
-        //camDirX = cameraJoystick.Horizontal * viewSpeed;
-        //camDirZ = cameraJoystick.Vertical * viewSpeed;
-
-        float horizontal = dirX;
-        float vertical = dirZ;
-
-        //store the direction
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        if (direction.magnitude >= 0.1f)
+        if (view.IsMine)
         {
-            //rotate to point towards where we are going
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            //movement
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * movementSpeed * Time.deltaTime);
-            
+            dirX = movementJoystick.Horizontal * movementSpeed;
+            dirZ = movementJoystick.Vertical * movementSpeed;
+
+            //camDirX = cameraJoystick.Horizontal * viewSpeed;
+            //camDirZ = cameraJoystick.Vertical * viewSpeed;
+
+            float horizontal = dirX;
+            float vertical = dirZ;
+
+            //store the direction
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+            if (direction.magnitude >= 0.1f)
+            {
+                //rotate to point towards where we are going
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(moveDir.normalized * movementSpeed * Time.deltaTime);
+
+            }
         }
+
+        
     }
 
-    private void FixedUpdate()
-    {
-        //rb.velocity = new Vector3(dirX, rb.velocity.y, dirZ);
-
-
-    }
 }
