00fb5f8954d86929df952ff672f3db8ee6497761
Hossain Alinaqi <hossain.alinaqi@ontariotechu.net>
2021-12-18 21:51:36 -0500

    got multiplayer to work

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
+using Cinemachine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,7 +11,12 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     //reference the camera
-    public Transform cam;
+    //public Transform cam;
+
+    public Camera cam;
+
+    //camera brain
+    public CinemachineFreeLook brain;
 
     //Initializes
     public Joystick movementJoystick;
@@ -27,11 +33,17 @@ public class PlayerMovement : MonoBehaviour
     float turnSmoothVelocity;
     public float turnSmoothTime;
 
+    //finding tag
+    GameObject joystickReference;
+    GameObject cameraReference;
+    GameObject cinemachineReference;
+
     //camera look at
     //private float camDirX, camDirZ, camDirY;
 
     //Photon
     public PhotonView view;
+    public PhotonTransformViewClassic transformView;
 
     // Start is called before the first frame update
     void Start()
@@ -40,8 +52,12 @@ public class PlayerMovement : MonoBehaviour
         //rb = GetComponent<Rigidbody>();
 
         //Photon
-        view = GetComponent<PhotonView>();
+        //view = GetComponentInParent<PhotonView>();
 
+        //transformView = GetComponentInParent<PhotonTransformViewClassic>();
+
+        view = GetComponent<PhotonView>();
+        transformView = GetComponent<PhotonTransformViewClassic>();
     }
 
     // Update is called once per frame
@@ -66,7 +82,7 @@ public class PlayerMovement : MonoBehaviour
             if (direction.magnitude >= 0.1f)
             {
                 //rotate to point towards where we are going
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
                 float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
@@ -78,5 +94,43 @@ public class PlayerMovement : MonoBehaviour
 
         
     }
+
+   
+    //Set data section
+    public void SetData(GameObject cameraParent)
+    {
+        //get and set references
+        joystickReference = GameObject.FindWithTag("Movement Joystick");
+        cameraReference = GameObject.FindWithTag("MainCamera");
+        cinemachineReference = GameObject.FindWithTag("Cinemachine Camera");
+
+        //set
+        movementJoystick = joystickReference.GetComponent<Joystick>();
+        /*
+        cam = cameraReference.GetComponent<Camera>();
+        CinemachineFreeLook brain = cinemachineReference.GetComponent<CinemachineFreeLook>();
+        brain.LookAt = this.transform;
+        brain.Follow = this.transform;
+        */
+        //movementJoystick[] tempJoystickList = cameraParent.GetComponent
+
+
+        //movementJoystick = cameraParent.transform.Find("Canvas").GetComponentInChildren<Joystick>();
+        //movementJoystick = cameraParent.transform.GetChild(2).GetComponentInChildren<Joystick>();
+        //movementJoystick = cameraParent.GameObject.FindGameObjectsWithTag("Movement Joystick");
+        //Joystick movementJoystick = cameraParent.GetComponentInChildren<Joystick>();
+        
+        cam = cameraParent.GetComponentInChildren<Camera>();
+        brain = cameraParent.GetComponentInChildren<CinemachineFreeLook>();
+        //CinemachineFreeLook brain = cameraParent.GetComponentInChildren<CinemachineFreeLook>();
+        brain.LookAt = this.transform;
+        brain.Follow = this.transform;
+        
+        //CinemachineVirtualCamera brain = cameraParent.GetComponentInChildren<CinemachineVirtualCamera>();
+
+
+    }
+
+
 
 }
--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -7,6 +7,8 @@ public class SpawnPlayers : MonoBehaviour
 {
     //Store player prefab
     public GameObject playerPrefab;
+    public GameObject cameraPrefab;
+    
 
     public float minX;
     public float maxX;
@@ -18,7 +20,18 @@ public class SpawnPlayers : MonoBehaviour
         //spawn location
         Vector3 spawnPosition = new Vector3(Random.Range(minX, maxX), 2.25f, Random.Range(minZ, maxZ));
         //instantiate object
-        PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition, Quaternion.identity);
+        //PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition, Quaternion.identity);
+        //PhotonNetwork.Instantiate(cameraPrefab.name, spawnPosition, Quaternion.identity);
+
+        //Set camera part up
+        GameObject test = PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition, Quaternion.identity);
+        if (test.GetComponent<PhotonView>().IsMine)
+        {
+            test.GetComponentInChildren<PlayerMovement>().SetData(Instantiate(cameraPrefab, spawnPosition, Quaternion.identity));
+        }
+        
     }
+
+
 
 }
